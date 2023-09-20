8c7e08c3c8a6757a6ec6af10d91723386a97c3d1
Francisco Flores <franciscojff25@gmail.com>
2023-09-20 19:46:29 -0300

    Co-authored-by: Cannibal03 <Cannibal03@users.noreply.github.com> Co-authored-by: Rusous <Rusous@users.noreply.github.com> Co-authored-by: DaryenCT <DaryenCT@users.noreply.github.com>

--- amuillang/src/App.js
+++ amuillang/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Prueba a ver si sirve</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testeando a lo shor si porque de inocente no se salva nadie
         </a>
       </header>
     </div>
